d496236cb1e67ae0d20ee5fa1a2e6da4028bdc68
Nursultan <abdyldaev93@gmail.com>
2018-09-11 16:45:17 -0700

    move http auth to application controller

--- app/controllers/growth/application_controller.rb
+++ app/controllers/growth/application_controller.rb
@@ -1,5 +1,7 @@
 module Growth
   class ApplicationController < ActionController::Base
+    http_basic_authenticate_with name: Growth.username, password: Growth.password unless Rails.env.test?
+
     protect_from_forgery with: :exception
   end
 end
