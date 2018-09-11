9611ba4ec205a9665bf8655be2e763fa3443798d
Nursultan <abdyldaev93@gmail.com>
2018-09-11 16:02:13 -0700

    add order to factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,4 +2,8 @@ FactoryBot.define do
   factory :customer do
     sequence(:email) { |n| "john.doe#{n}@example.com" }
   end
+
+  factory :order do
+    sequence(:title) { |n| "order ##{n}" }
+  end
 end
