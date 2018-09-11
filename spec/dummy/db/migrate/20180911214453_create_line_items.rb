5d8031665235fb422f95272377a26e572b76beaf
Nursultan <abdyldaev93@gmail.com>
2018-09-11 14:49:46 -0700

    add LineItem model

---
+++ spec/dummy/db/migrate/20180911214453_create_line_items.rb
@@ -0,0 +1,10 @@
+class CreateLineItems < ActiveRecord::Migration[5.1]
+  def change
+    create_table :line_items do |t|
+      t.references :product, foreign_key: true
+      t.references :order, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
