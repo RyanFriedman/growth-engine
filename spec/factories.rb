4a59253a882470583e2c41d072e7929e1573be94
Nursultan <abdyldaev93@gmail.com>
2018-09-12 19:10:02 -0700

    add customer_with_order factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,6 +3,16 @@ FactoryBot.define do
     sequence(:email) { |n| "john.doe#{n}@example.com" }
   end
 
+  factory :customer_with_order, class: Customer do
+    transient do
+      orders_count { 1 }
+
+      after(:create) do |customer, evaluator|
+        create_list(:order, evaluator.orders_count, customer: customer)
+      end
+    end
+  end
+
   factory :order do
     sequence(:title) { |n| "order ##{n}" }
   end
