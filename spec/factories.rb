53c9598142963d461c971a396d810127b37529e5
Nursultan <abdyldaev93@gmail.com>
2018-09-13 13:55:03 -0700

    allow to pass order_created_at for customer_with_order factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,9 +5,10 @@ FactoryBot.define do
     factory :customer_with_order do
       transient do
         orders_count { 1 }
+        order_created_at { Date.current }
 
         after(:create) do |customer, evaluator|
-          create_list(:order, evaluator.orders_count, customer: customer)
+          create_list(:order, evaluator.orders_count, customer: customer, created_at: evaluator.order_created_at)
         end
       end
     end
