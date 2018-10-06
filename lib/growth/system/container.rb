85a61e994bcdbc2efa892cbbd52aefcacea7fe1a
Nursultan <abdyldaev93@gmail.com>
2018-10-05 20:20:27 -0700

    define container for objects dependency

---
+++ lib/growth/system/container.rb
@@ -0,0 +1,37 @@
+require "dry/transaction/operation"
+
+require_relative '../operations/retention_report/validate'
+require_relative '../operations/retention_report/prepare'
+require_relative '../operations/retention_report/generate'
+
+module Growth
+  module System
+    class Container
+      extend Dry::Container::Mixin
+
+      namespace "growth" do
+        namespace "operations" do
+          namespace "retention_report" do
+            register "validate" do
+              Growth::Operations::RetentionReport::Validate.new
+            end
+
+            register "prepare" do
+              Growth::Operations::RetentionReport::Prepare.new
+            end
+
+            register "generate" do
+              Growth::Operations::RetentionReport::Generate.new
+            end
+          end
+        end
+
+        namespace "transactions" do
+          register "retention_report" do
+            Growth::Transactions::RetentionReport.new
+          end
+        end
+      end
+    end
+  end
+end
