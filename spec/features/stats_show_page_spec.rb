3456dea61955c761e461b84d17b59edc3b117e37
Nursultan <abdyldaev93@gmail.com>
2018-10-10 12:52:09 -0700

    move customer page spec to stats show page specs

--- spec/features/stats_show_page_spec.rb
+++ spec/features/stats_show_page_spec.rb
@@ -1,11 +1,59 @@
 require 'rails_helper'
 
 describe 'stats show page' do
-  it 'displays monthly resource count' do
-    
+  let(:resource) {'customer'}
+
+  before do
+    visit_stats_show_page(resource: resource)
   end
-  
-  it 'displays daily resource count' do
-    
+
+  it 'displays daily resource count'
+
+  it 'displays monthly resource count'
+
+  it 'displays yearly resource count'
+
+  it 'shows retention report' do
+    create_list(:customer, 2)
+
+    create(:customer_with_order)
+    create(:customer_with_order, orders_count: 2)
+
+    click_link('Order')
+
+    expect(page).to have_content('Customers that have Orders: 2')
+  end
+
+  it 'displays sub resources total count' do
+    create(:customer_with_order)
+    create(:customer_with_order, orders_count: 2)
+
+    click_link('Order')
+
+    expect(page).to have_content('Total Orders created: 3')
+  end
+
+  it 'displays retention data for given resources' do
+    create(:customer)
+
+    create_list(:customer_with_order, 2)
+    create_list(:customer_with_order, 3, orders_count: 2)
+
+    click_link('Order')
+
+    expect(page).to have_content('2 Customers have 1 order (40.0% of Customers)')
+    expect(page).to have_content('3 Customers have 2 orders (60.0% of Customers)')
+  end
+
+  it 'shows export to csv links' do
+    create(:customer)
+
+    create_list(:customer_with_order, 2)
+    create_list(:customer_with_order, 3, orders_count: 2)
+
+    click_link('Order')
+
+    expect(page).to have_link('export emails to csv', href: '/growth/stats/Customer.csv?association=Customer-Order&source_resources_count=2&target_resources_count=1')
+    expect(page).to have_link('export emails to csv', href: '/growth/stats/Customer.csv?association=Customer-Order&source_resources_count=3&target_resources_count=2')
   end
 end
--- spec/support/features/navigation_helpers.rb
+++ spec/support/features/navigation_helpers.rb
@@ -2,4 +2,8 @@ module NavigationHelpers
   def visit_stats_page
     visit '/growth/stats'
   end
+
+  def visit_stats_show_page(resource:)
+    visit '/growth/stats/' + resource
+  end
 end
