4cc4a27d0655dcb5872c2bc0393b1b8606eb4f25
Ryan Friedman <ryan@soundtribe.com>
2018-10-11 16:55:48 -0700

    Update initializer

--- lib/generators/templates/growth.rb
+++ lib/generators/templates/growth.rb
@@ -2,20 +2,20 @@ Growth.setup do |config|
   # Default: Measures all models.
   # models_to_measure takes an array of Rails models, for example:
   #
-  # Growth.models_to_measure = [User, Product, Order, Payment]
+  # Growth.models_to_measure = [ User, Product, Order, Payment ]
   
   # Default: Prevent specific models from being measured
   # model_blacklist takes an array of Rails models, for example:
   #
-  # Growth.model_blacklist = [AdminUser]
+  # Growth.model_blacklist = [ AdminUser ]
   
-  # This is your username for securing the '/stats' URL
+  # This is your username for securing the '/growth' route
   # You will be prompted to enter this when viewing the page
   # This value would be better stored in an environment variabe
   #
   # Growth.username = ENV.fetch[:growth_username]
   
-  # This is your password for securing the /stats page
+  # This is your password for securing the '/growth' route
   # You will be prompted to enter this when viewing the page
   # This value would be better stored in an environment variabe
   #
